6a2c23d323a5b2f2b9f726f9d7e4905e49917ec1
Jekaterina Suvorova <kateupgo@gmail.com>
2022-06-01 13:01:17 +0300

    Initial commit

--- src/test/java/homeWork5/Shop1aTest.java
+++ src/test/java/homeWork5/Shop1aTest.java
@@ -1,11 +1,10 @@
 package homeWork5;
 
-import jdk.swing.interop.SwingInterOpUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.MoveTargetOutOfBoundsException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,8 +20,8 @@ public class Shop1aTest {
     private final By MENU_LINK = By.xpath(".//a[@class = 'submenu-lvl1__link']");
     private final By LEFT_SUBMENU = By.xpath(".//ul[@class = 'menu product-categories-menu']/li/a");
     private final By LEFT_SUB_SUBMENU = By.xpath(".//a[@class = 'list-filterable__label']");
-    private final By ITEM = By.xpath(".//a[@class = 'catalog-taxons-product__name']");
-    //private final By ITEM_LINK = By.xpath("a[@class = 'catalog-taxons-product__image-anchor']");
+    private final By ITEM = By.xpath(".//div[contains(@class, 'catalog-taxons-product--grid-view')]");
+    private final By ITEM_NAME = By.xpath(".//a[@class = 'catalog-taxons-product__name']");
     private final By ITEM_PRICE = By.xpath(".//div[@class = 'product-price-details__block']/span[@class = 'price']");
 
     private final String CATEGORY_NAME = "Auto un moto preces";
@@ -78,19 +77,20 @@ public class Shop1aTest {
         }
 
         wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(ITEM, 1));
-
         List<WebElement> items = browser.findElements(ITEM);
+
         Random rand = new Random();
         int randomItem = rand.nextInt(items.size());
 
         Actions action = new Actions(browser);
-        action.moveToElement(browser.findElement(ITEM)).build().perform();
 
         try {
-            items.get(randomItem).click();
-        } catch (ElementClickInterceptedException e) {
-            items.get(randomItem).click();
+            action.moveToElement(items.get(randomItem)).perform();
+        } catch (MoveTargetOutOfBoundsException e) {
+            action.pause(3000).moveToElement(items.get(randomItem)).perform();
         }
+
+        items.get(randomItem).findElement(ITEM_NAME).click();
 
         WebElement price = browser.findElement(ITEM_PRICE);
         System.out.println("Price: " + price.getText());
